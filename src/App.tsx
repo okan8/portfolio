f5b0246cd2cd8ec89dd4c8cf1832e8767ef6fdf7
okan8 <okan@siberonculer.com>
2025-02-14 19:19:25 +0300

    main

--- src/App.tsx
+++ src/App.tsx
@@ -228,21 +228,25 @@ function App() {
             </div>
           </div>
         </div>
+        
       )}
 
       {/* Tawk.to Integration */}
       <script
         dangerouslySetInnerHTML={{
-          __html: `
-            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-            (function(){
-              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-              s1.async=true;
-              s1.src='https://embed.tawk.to/67af6c1d1abee4190cd2a8c1/1ik2jsl2n';
-              s1.charset='UTF-8';
-              s1.setAttribute('crossorigin','*');
-              s0.parentNode.insertBefore(s1,s0);
-            })();
+          __html: `<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/67af6c1d1abee4190cd2a8c1/1ik2jsl2n';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
           `
         }}
       />
